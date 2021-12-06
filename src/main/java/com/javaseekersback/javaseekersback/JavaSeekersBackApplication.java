3526429c7091fb76db8b770b8c52659d624da315
Dmytro Savchuk <dsavchuk@agileassets.com>
2021-12-06 16:23:03 +0200

    Initial commit

---
+++ src/main/java/com/javaseekersback/javaseekersback/JavaSeekersBackApplication.java
@@ -0,0 +1,14 @@
+package com.javaseekersback.javaseekersback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class JavaSeekersBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSeekersBackApplication.class, args);
+    }
+}
---
+++ src/test/java/com/javaseekersback/javaseekersback/JavaSeekersBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaseekersback.javaseekersback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSeekersBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
